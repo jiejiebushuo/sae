e5944562f2a32d86f3bcebdc47812dd2486f7685
xiaocong <xiepinxi@gmail.com>
2013-09-25 18:20:20 +0800

    更新数据库名记录入库

--- public/saemysqlbak/sqlbak.php
+++ public/saemysqlbak/sqlbak.php
@@ -3,6 +3,7 @@ $mysql_table_name = "deferredjob_log"; //要存储备份记录的表名
 $bakphp_dir = "mysqlbak/";  //你把备份的php 放在哪个目录了 根目录请为空 不要加/;
 $bak_Storagename = "mysqlbakStoragename";  //备份下来的数据库放在哪个Storag 中,一定要是一个公开是的Storag
 $sqlbak_key = "okklkjaksjdk"; // 这可以一段随意的字符串,只有知道此字符串才能执行添加备份任务 (字母加数字组合)
+$dbname = SAE_MYSQL_DB;
 
 /*
 注意:自动删除七天前备份,可以在下面配置更长. 定时任务方式大概会在第二天的1点左右开始备份
@@ -42,23 +43,17 @@ $dj = new SaeDeferredJob();
 /* 连主库 */
 $link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
 
-if(isset($_GET["install"])){ //执行新建表
-	$sql = "
-	CREATE TABLE ".$mysql_table_name." (
- 	 	id int(8) NOT NULL COMMENT '任务id',
-  		sqlname varchar(50) DEFAULT NULL COMMENT '备份文件名',
-  		addtime datetime DEFAULT NULL COMMENT '任务添加时间',
-  		stat int(1) DEFAULT NULL COMMENT '状态码:3备份成功;-2为已删除; 其它参考SaeDeferredJob API使用介绍',
- 	 	overtime datetime DEFAULT NULL COMMENT '完成时间'
-	) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
-	
+if(isset($_GET["install"])){ //执行新建表 
+	//$sql = "CREATE TABLE `".$mysql_table_name."` ( `id` int(8) NOT NULL COMMENT '任务id', `sqlname` varchar(50) DEFAULT NULL COMMENT '备份文件名',	`dbname` varchar(50) DEFAULT NULL COMMENT '数据库名', `addtime` datetime DEFAULT NULL COMMENT '任务添加时间', `stat` int(1) DEFAULT NULL COMMENT '状态码:3备份成功\-2为已删除 其它参考SaeDeferredJob API使用介绍', `overtime` datetime DEFAULT NULL COMMENT '完成时间' ) ENGINE=MyISAM DEFAULT CHARSET=utf8";
+	$sql = "CREATE TABLE `" . $mysql_table_name . "` ( `id` int(8) NOT NULL COMMENT '任务id', `sqlname` varchar(50) DEFAULT NULL COMMENT '备份文件名', `dbname` varchar(50) DEFAULT NULL COMMENT '数据库名', `addtime` datetime DEFAULT NULL COMMENT '任务添加时间', `stat` int(1) DEFAULT NULL COMMENT '状态码:3备份成功;-2为已删除; 其它参考SaeDeferredJob API使用介绍', `overtime` datetime DEFAULT NULL COMMENT '完成时间' ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
 	if(mysql_num_rows(mysql_query("SHOW TABLES LIKE '".$mysql_table_name."'")==1)){
 		echo "数据表已存在,请确认.";	
 	}else{
-		if($rql = mysql_query($sql,$link)){
-			echo "数据表新建成功";	
+		if($link && $rql = mysql_query($sql,$link)){
+			echo "数据表新建成功";	 
 		}else{
-			echo "数据库新建失败,请刷新重试";	
+			echo $sql;
+			echo "数据表新建失败,请刷新重试";	
 		}
 	}
 }
@@ -66,15 +61,15 @@ if(isset($_GET["install"])){ //执行新建表
 if( isset($_GET["sqlbak"]) && $_GET["sqlbak"] === $sqlbak_key){
 	 //添加任务
 	 $nowtime = date("YmdHms");
-	 $sqlname = $nowtime.SAE_MYSQL_DB."bak.sql.zip";
-	 $callbackurl = $bakphp_dir."sqlbak.php?sqlbakcallback=".$nowtime.SAE_MYSQL_DB;
-	 $taskID=$dj->addTask("export","mysql",$bak_Storagename,$sqlname,SAE_MYSQL_DB,NULL,$callbackurl);
+	 $sqlname = $nowtime.$dbname."bak.sql.zip";
+	 $callbackurl = $bakphp_dir."sqlbak.php?sqlbakcallback=".$nowtime;
+	 $taskID=$dj->addTask("export","mysql",$bak_Storagename,$sqlname,$dbname,NULL,$callbackurl);
 	 if($taskID===false){
 		 var_dump($dj->errno(), $dj->errmsg());
 	 }else{
 		echo "备份任务添加成功,任务id:".$taskID;
-		mysql_select_db(SAE_MYSQL_DB,$link);
-		$sql = "INSERT INTO `".$mysql_table_name."` (`id`, `sqlname`, `addtime`, `stat`, `overtime`) VALUES ('".$taskID."', '".$sqlname."', now(), '0', '');";
+		mysql_select_db($dbname,$link);
+		$sql = "INSERT INTO `".$mysql_table_name."` (`id`, `sqlname`, `dbname`,`addtime`, `stat`, `overtime`) VALUES ('".$taskID."', '".$sqlname."','" . $dbname . "', now(), '0', '');";
         if($link && $rql = mysql_query($sql,$link))
 		{
 			echo "入库成功.";	
@@ -88,7 +83,7 @@ if( isset($_GET["sqlbak"]) && $_GET["sqlbak"] === $sqlbak_key){
 				var_dump($dj->errno(), $dj->errmsg());
 			}else{
 				echo "删除任务完成";
-				$sql = "UPDATE  `".$mysql_table_name."` SET  `stat` = '-1' , `overtime` = NOW() WHERE `deferredjob_log`.`id` ='".$taskID."'";
+				$sql = "UPDATE  `".$mysql_table_name."` SET  `stat` = '-1' , `overtime` = NOW() WHERE `id` ='".$taskID."' AND `dbname` = '" . $dbname . "';";
 				if($link && $rql = mysql_query($sql,$link))
 				{
 					echo "修改记录成功.";		
@@ -102,8 +97,8 @@ if( isset($_GET["sqlbak"]) && $_GET["sqlbak"] === $sqlbak_key){
 }
 if( isset($_GET["sqlbakcallback"])){
 	//查看状态
-	$sql ="SELECT * FROM  `".$mysql_table_name."` WHERE `sqlname` LIKE  '%".$_GET["sqlbakcallback"]."%' LIMIT 1";
-	mysql_select_db(SAE_MYSQL_DB,$link);
+	$sql ="SELECT * FROM  `".$mysql_table_name."` WHERE `sqlname` LIKE  '%".$_GET["sqlbakcallback"]."%' AND  `dbname` = '" . $dbname . "' LIMIT 1";
+	mysql_select_db($dbname,$link);
 	if($link && $rql = mysql_query($sql,$link))
 	{
 		$row = mysql_fetch_row($rql);
@@ -137,16 +132,16 @@ if( isset($_GET["sqlbakcallback"])){
 					break ;
 			}	
 		}
-		$sql = "UPDATE `".$mysql_table_name."` SET  `stat` = '$start' , `overtime` = NOW() WHERE `".$mysql_table_name."`.`id` ='".$row["0"]."'";
+		$sql = "UPDATE `".$mysql_table_name."` SET  `stat` = '$start' , `overtime` = NOW() WHERE `id` ='".$row["0"]."' AND  `dbname` = '" . $dbname . "'";
 		if($rql = mysql_query($sql,$link)){
 			echo $row["0"]."入库成功.";
-			$sql = "SELECT DISTINCT * FROM  `".$mysql_table_name."` WHERE  `stat` = 3 ORDER BY  `id` DESC LIMIT 7 , 30"; //删除7天前成功备份 可以在这里设置留多少天的备份
+			$sql = "SELECT DISTINCT * FROM `".$mysql_table_name."` WHERE  `stat` = 3 AND  `dbname` = '" . $dbname . "' ORDER BY  `id` DESC LIMIT 7 , 30"; //删除7天前成功备份 可以在这里设置留多少天的备份
 			if($rqlold = mysql_query($sql,$link)){
 				$s = new SaeStorage();
 				while($rowold=mysql_fetch_array($rqlold,MYSQL_ASSOC)){
 					$dellstat = $s->delete($bak_Storagename,$rowold["sqlname"]);
 					if($dellstat){
-						$sql = "UPDATE `".$mysql_table_name."` SET  `stat` = '-2' , `overtime` = NOW() WHERE `".$mysql_table_name."`.`id` ='".$rowold["id"]."'";
+						$sql = "UPDATE `".$mysql_table_name."` SET  `stat` = '-2' , `overtime` = NOW() WHERE `id` ='".$rowold["id"]."' AND  `dbname` = '" . $dbname . "'";
 						if(mysql_query($sql,$link)){
 							echo "删除".$rowold["overtime"]."成功";
 							echo "<br>";
